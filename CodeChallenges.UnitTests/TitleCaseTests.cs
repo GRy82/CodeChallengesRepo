f93edc4621a8c41ecb474b1d02fd6f82ff3c88ba
GRy82 <gwryan@buffalo.edu>
2021-05-25 13:53:01 -0400

    testing project added to the solution.  Outline of tests created for TitleCaseChallenge.

---
+++ CodeChallenges.UnitTests/TitleCaseTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeChallenges;
+
+namespace CodeChallenges.UnitTests
+{
+    [TestClass]
+    public class TitleCaseTests
+    {
+        [TestMethod]
+        public void TitleCase_HappyPath_EqualStrings()
+        {
+        }
+    }
+}
