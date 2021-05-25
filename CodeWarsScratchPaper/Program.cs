93a0fd6ffffbf385c9b24838099c7d22db690d69
GRy82 <gwryan@buffalo.edu>
2021-05-25 09:54:41 -0400

    'brute force' method of solving 'TitleCase' implemented. See comments for explanation of problem.

--- CodeWarsScratchPaper/Program.cs
+++ CodeWarsScratchPaper/Program.cs
@@ -36,10 +36,35 @@ namespace CodeWarsScratchPaper
             //var thing = productFib(714);
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            
+            Console.WriteLine(TitleCase("aBc dEf gHi", null));
             stopwatch.Stop();
             Console.WriteLine(stopwatch.ElapsedMilliseconds + " ms elapsed.");
             Console.Read();
+        }
+
+
+        // First argument is title to convert to 'title case'. Second argument is string of words separated by commas,
+        // each of which should not be capitalized unless it's the first word of the title.
+        public static string TitleCase(string title, string minorWords = "")
+        {
+            string titleString = title.ToLower();
+            string[] minorWordsArray = minorWords != null && minorWords.Length > 0 ? 
+                minorWords.ToLower().Trim().Split(' ') : new string[0];
+            string[] titleWords = titleString.Split(' ');
+            for (int i = 0; i < titleWords.Length; i++)
+            {
+                if (titleWords[i].Length < 1) continue;
+                if (!minorWordsArray.Contains(titleWords[i]) || i == 0)
+                {
+                    char firstLetter = titleWords[i][0];
+                    firstLetter = Char.ToUpper(firstLetter);
+                    char[] charArray = titleWords[i].ToCharArray();
+                    charArray[0] = firstLetter;
+                    titleWords[i] = String.Join("", charArray);
+                }
+            }
+
+            return string.Join(" ", titleWords);
         }
 
         public static ulong Fib(ulong n)
