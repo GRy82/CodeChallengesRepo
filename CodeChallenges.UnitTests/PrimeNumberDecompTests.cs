128a42cee8561a47a6e20412d9933c33f7b35b33
GRy82 <gwryan@buffalo.edu>
2021-05-26 13:37:12 -0400

    class and method set up for prime number decomposition, as well as a testing class, and its testing methods.

---
+++ CodeChallenges.UnitTests/PrimeNumberDecompTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeChallenges;
+
+namespace CodeChallenges.UnitTests
+{
+    [TestClass]
+    public class PrimeNumberDecompTests
+    {
+        [TestMethod]
+        public void Factors_ArgLessThanOne_ReturnMessage()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void Factors_PrimeArg_ReturnSelfAsString()
+        {
+
+        }
+
+        [TestMethod]
+        public void Factors_NonPrimeArg_ReturnInTypicalFormat()
+        {
+
+        }
+    }
+}
---
+++ CodeChallenges/PrimeNumberDecomposition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges
+{
+    public class PrimeNumberDecomposition
+    {
+        public static string Factors(int lst)
+        {
+            if (lst < 1) return "You must enter a number greater than '1'";
+
+            return PrimeFactor(lst, new StringBuilder(""));
+        }
+
+        public static string PrimeFactor(int lst, StringBuilder currentString)
+        {
+
+            return "";
+        }
+    }
+}
