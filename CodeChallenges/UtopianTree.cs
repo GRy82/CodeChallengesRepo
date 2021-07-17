70c44c89799c9a10d7752f9dab3af17ff7875b06
GRy82 <gwryan@buffalo.edu>
2021-07-17 11:56:20 -0400

    See comments for description of challenge, and the coded solution below that.

---
+++ CodeChallenges/UtopianTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges
+{
+
+    // The Utopian Tree goes through 2 cycles of growth every year.Each spring, it doubles in height.Each summer, its height increases by 1 meter.
+    // A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring.How tall will the tree be after  growth cycles?
+    public class UtopianTree
+    {
+        public int utopianTree(int growthCycles)
+        {
+            int treeHeight = 1;
+            bool isSpring = true;
+            while(growthCycles > 0)
+            {
+                if (isSpring) treeHeight *= 2;
+                else treeHeight += 1;
+                isSpring = !isSpring;
+                growthCycles--;
+            }
+            return treeHeight;
+        }
+    }
+}
