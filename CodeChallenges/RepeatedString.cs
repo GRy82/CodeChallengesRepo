c8db35eec00c76388224894bc51ce57a626fe6bd
GRy82 <gwryan@buffalo.edu>
2021-08-09 13:31:44 -0400

    see added class for a newly completed coding challenge. Description of the challenge can be seen in the form of comments, and the coded out solution to the problem can be seen below that.

---
+++ CodeChallenges/RepeatedString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges
+{
+    // There is a string, s, of lowercase English letters that is repeated infinitely many times.
+    // Given an integer, n, find and print the number of letter a's in the first n letters of the infinite string.
+    public class RepeatedString
+    {
+        public static long repeatedString(string s, long n)
+        {
+            long completedReps = n / s.Length;
+            int leftoverLetters = (int)(n % s.Length);
+            int qtyOfAInS = getAsInS(s);
+            return qtyOfAInS * completedReps + getAsInS(s.Substring(0, leftoverLetters));
+        }
+
+        static int getAsInS(string s)
+        {
+            int qtyOfA = 0;
+            foreach (char character in s)
+                if (character == 'a')
+                    qtyOfA++;
+
+            return qtyOfA;
+        }
+    }
+}
