aef757aa0e21e5fed55c475ea6197fd0f1f1af84
GRy82 <gwryan@buffalo.edu>
2021-08-10 12:37:58 -0400

    Coding challenge completed. See added class for a description of the challenge in the form of comments. and the coded out solution below it.

---
+++ CodeChallenges/EqualizeArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges
+{
+    // Given an array of integers, determine the minimum number of elements to delete to leave only elements of equal value.
+    public class EqualizeArray
+    {
+        public static int EqualizeTheArray(List<int> arr)
+        {
+            Dictionary<int, int> intOccurrences = new Dictionary<int, int>();
+            foreach (var number in arr)
+            {
+                bool keyExists = intOccurrences.TryAdd(number, 1);
+                if (!keyExists)
+                    intOccurrences[number]++;
+            }
+
+            int maxOccurrences = 0;
+            foreach (var key in intOccurrences.Keys)
+                if (intOccurrences[key] > maxOccurrences)
+                    maxOccurrences = intOccurrences[key];
+
+            return arr.Count - maxOccurrences;
+        }
+    }
+}
--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -11,7 +11,7 @@ namespace CodeChallenges
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            Console.WriteLine(NonDivisibleSubset.nonDivisibleSubset(7, new List<int> { 278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436 }));
+            Console.WriteLine(EqualizeArray.EqualizeTheArray(new List<int> { 3, 3, 2, 1, 3 }));
             stopwatch.Stop();
             Console.WriteLine(stopwatch.ElapsedMilliseconds + " ms elapsed.");
             Console.ReadLine();
