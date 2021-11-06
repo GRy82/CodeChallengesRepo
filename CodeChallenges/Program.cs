dbef9216db560f3f2fed4d6b8fd3f1e7b2b07dde
GRy82 <gwryan@buffalo.edu>
2021-11-06 10:23:08 -0500

    Coding challenge completed. See the added class for the description of the problem in the form of comments, and the coded solution that follows.

--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -11,17 +11,19 @@ namespace CodeChallenges
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            Console.WriteLine(QueensAttack.queensAttack(88587, 9, 20001, 20003, new List<List<int>> { 
-                new List<int>{ 20001, 20002},
-                new List<int>{ 20001, 20004},
-                new List<int>{ 20000, 20003},
-                new List<int>{ 20002, 20003},
-                new List<int>{ 20000, 20004},
-                new List<int>{ 20000, 20002},
-                new List<int>{ 20002, 20004},
-                new List<int>{ 20002, 20002},
-                new List<int>{ 564, 323},
-            }));
+            //Console.WriteLine(QueensAttack.queensAttack(88587, 9, 20001, 20003, new List<List<int>> { 
+            //    new List<int>{ 20001, 20002},
+            //    new List<int>{ 20001, 20004},
+            //    new List<int>{ 20000, 20003},
+            //    new List<int>{ 20002, 20003},
+            //    new List<int>{ 20000, 20004},
+            //    new List<int>{ 20000, 20002},
+            //    new List<int>{ 20002, 20004},
+            //    new List<int>{ 20002, 20002},
+            //    new List<int>{ 564, 323},
+            //}));
+            var answer = TopicsKnown.acmTeam(new List<string> { "10101", "11100", "11010", "00101" });
+            Console.WriteLine("highest number of topics known by a team: " + answer[0] + "\nnumber of teams knowing that many topics: " + answer[1]);
             stopwatch.Stop();
             Console.WriteLine(stopwatch.ElapsedMilliseconds + " ms elapsed.");
             Console.ReadLine();
---
+++ CodeChallenges/TopicsKnown.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges
+{
+    public static class TopicsKnown
+    {
+        /*
+        There are a number of people who will be attending ACM-ICPC World Finals.Each of them may be well versed in a number 
+        of topics.Given a list of topics known by each attendee, presented as binary strings, determine the maximum number 
+        of topics a 2-person team can know. Each subject has a column in the binary string, and a '1' means the subject
+        is known while '0' means it is not.Also determine the number of teams that know the maximum number of topics.
+        Return an integer array with two elements.The first is the maximum number of topics known, and the second is 
+        the number of teams that know that number of topics.
+        */
+
+        public static List<int> acmTeam(List<string> topicsSet)
+        {
+            Dictionary<int[], int> subjectsKnownByPair = new Dictionary<int[], int>();
+
+            /* The way the nested loop is setup, we will never encounter any repeat pairs that are just ordered differently
+            *  Also, no team member will ever be attempted to be "paired" with themselves.
+            */
+            for (int i = 0; i < topicsSet.Count; i++)
+            {
+                for (int j = i + 1; j < topicsSet.Count; j++)
+                {
+                    int[] pair = { i, j };
+                    subjectsKnownByPair[pair] = topicsKnownByTeam(topicsSet, i, j);
+                }
+            }
+
+            return MaxTopicsKnownAndFrequencyOfMax(subjectsKnownByPair);
+        }
+
+        private static int topicsKnownByTeam(List<String> topicsSet, int memberOne, int memberTwo)
+        {
+            int numberOfTopicsKnown = 0;
+
+            char[] topicsSetCharArrOne = topicsSet[memberOne].ToCharArray();
+            char[] topicsSetCharArrTwo = topicsSet[memberTwo].ToCharArray();
+            for (int i = 0; i < topicsSet[0].Length; i++)
+            {
+                if (topicsSetCharArrOne[i] == '1' || topicsSetCharArrTwo[i] == '1')
+                    numberOfTopicsKnown++;
+            }
+
+            return numberOfTopicsKnown;
+        }
+
+        private static List<int> MaxTopicsKnownAndFrequencyOfMax(Dictionary<int[], int> subjectsKnownByPair)
+        {
+            int maxSubjectsKnown = 0;
+            int maxSubjectsKnownFrequency = 0;
+
+            foreach (var pair in subjectsKnownByPair.Keys)
+            {
+                if (subjectsKnownByPair[pair] > maxSubjectsKnown)
+                {
+                    maxSubjectsKnown = subjectsKnownByPair[pair];
+                    maxSubjectsKnownFrequency = 1;
+                }
+                else if (subjectsKnownByPair[pair] == maxSubjectsKnown)
+                    maxSubjectsKnownFrequency++;
+            }
+
+            return new List<int> { maxSubjectsKnown, maxSubjectsKnownFrequency };
+        }
+    }
+}
